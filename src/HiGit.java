bf13535c5b192d78fb0d3be5c2c9a86a926d4d00
llf <llf@qq.com>
2020-03-25 09:31:12 +0800

    DD01

--- src/HiGit.java
+++ src/HiGit.java
@@ -2,5 +2,6 @@ public class HiGit {
     public static void main(String args[]){
         System.out.println("HiGit");
         System.out.println("HiGit1113333");
+        System.out.println("DD");
     }
 }
